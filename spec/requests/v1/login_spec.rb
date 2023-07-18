310684c77ef9fe76d741d9168ce3da5b4ccbb51c
koba-masa <kobayashi_mshr@koba-masa.com>
2023-07-18 21:47:24 +0900

    feat: ログイン成功時のHTTPステータスをOKに変更

--- spec/requests/v1/login_spec.rb
+++ spec/requests/v1/login_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe 'V1::Logins' do
     context 'when login_id and password are correct' do
       let(:params) { { login_id: valid_user.login_id, password: valid_user.password } }
 
-      it 'returns http created' do
+      it 'returns http status ok' do
         login
-        expect(response).to have_http_status(:created)
+        expect(response).to have_http_status(:ok)
         expect(response.parsed_body['token']).to be_present
       end
     end
