51d914876f4d1873c5c2137cbb06b236a2edc477
koba-masa <kobayashi_mshr@koba-masa.com>
2023-07-30 11:12:09 +0900

    feat: 取引履歴テーブルの構造変更に伴うControllerの修正

--- app/controllers/v1/dealings_controller.rb
+++ app/controllers/v1/dealings_controller.rb
@@ -4,7 +4,7 @@ module V1
   class DealingsController < ApplicationController
     before_action :check_authentication
 
-    def create
+    def create # rubocop:disable Metrics/MethodLength
       matching = VMatching.untrading.find_candidate_matching!(
         current_user.id,
         applicant_want_id,
@@ -13,14 +13,27 @@ module V1
         applicant_stock_id,
       )
 
-      ::Dealing.applicate(
-        current_user.id,
-        matching.your_user_id,
-        matching.my_want_id,
-        matching.your_stock_id,
-        matching.your_want_id,
-        matching.my_stock_id,
-      )
+      ActiveRecord::Base.transaction do
+        applicant_dealing = ::Dealing.applicate(
+          current_user,
+          current_user,
+          matching.your_user_id,
+          matching.my_want_id,
+          matching.your_stock_id,
+          nil,
+        )
+
+        partner_dealing = ::Dealing.applicate(
+          current_user,
+          matching.your_user_id,
+          current_user,
+          matching.your_want_id,
+          matching.my_stock_id,
+          applicant_dealing,
+        )
+
+        applicant_dealing.update(dealing: partner_dealing)
+      end
 
       render json: {}, status: :ok
     rescue ActiveRecord::RecordNotFound
@@ -28,17 +41,39 @@ module V1
       render json: { errors: { message: '状態が更新されてしまいました' } }, status: :not_found
     end
 
+    # TODO: 申請者が承認できないようにする
     def approve
       dealing = ::Dealing.find(params[:id])
-      dealing.approve(current_user)
+      concerned_dealings = dealing.dealings
+
+      ActiveRecord::Base.transaction do
+        ::Dealing.lock.find(concerned_dealings)
+        dealing.approve
+
+        approval_count = concerned_dealings.count(&:approval?)
+
+        if approval_count == (concerned_dealings.count - 1)
+          concerned_dealings.each do |concerned_dealing|
+            concerned_dealing.update(status: :trading)
+          end
+        end
+      end
+
       render json: {}, status: :ok
     rescue StandardError
       render json: { errors: {} }, status: :forbidden
     end
 
+    # TODO: 申請者が拒否できないようにする
     def destroy
       dealing = ::Dealing.find(params[:id])
-      dealing.deny(current_user)
+      concerned_dealings = dealing.dealings
+
+      ActiveRecord::Base.transaction do
+        ::Dealing.lock.find(concerned_dealings)
+        concerned_dealings.each(&:deny)
+      end
+
       render json: {}, status: :ok
     rescue StandardError
       render json: { errors: {} }, status: :forbidden
--- spec/requests/v1/dealings_spec.rb
+++ spec/requests/v1/dealings_spec.rb
@@ -8,16 +8,35 @@ RSpec.describe 'V1::Dealings' do
       'Authorization' => "Bearer #{login(user.id)}",
     }
   end
+
   let(:applicant) { create(:user) }
   let(:partner) { create(:user, display_name: '別ユーザ') }
+  let(:other_partner) { create(:user) }
+
   let(:category) { create(:category) }
   let(:good) { create(:good, category:) }
   let(:character1) { create(:character, good:) }
   let(:character2) { create(:character, good:) }
-  let(:applicant_want) { create(:want, user: applicant, character: character1) }
-  let(:applicant_stock) { create(:stock, user: applicant, character: character2) }
-  let(:partner_want) { create(:want, user: partner, character: character2) }
-  let(:partner_stock) { create(:stock, user: partner, character: character1) }
+  let(:character3) { create(:character, good:) }
+
+  let(:applicant_want1) { create(:want, user: applicant, character: character1) }
+  let(:applicant_stock1) { create(:stock, user: applicant, character: character2) }
+  let(:partner_want1) { create(:want, user: partner, character: character2) }
+  let(:partner_stock1) { create(:stock, user: partner, character: character1) }
+
+  let(:applicant_dealing1) { create(:dealing, applicant:, want: applicant_want1, stock: partner_stock1) }
+  let(:partner_dealing1) { create(:dealing, applicant:, want: partner_want1, stock: applicant_stock1) }
+
+  let(:applicant_want2) { create(:want, user: applicant, character: character1) }
+  let(:applicant_stock2) { create(:stock, user: applicant, character: character2) }
+  let(:partner_want2) { create(:want, user: partner, character: character2) }
+  let(:partner_stock2) { create(:stock, user: partner, character: character3) }
+  let(:other_partner_want) { create(:want, user: partner, character: character3) }
+  let(:other_partner_stock) { create(:stock, user: partner, character: character1) }
+
+  let(:applicant_dealing2) { create(:dealing, applicant:, want: applicant_want2, stock: other_partner_stock) }
+  let(:partner_dealing2) { create(:dealing, applicant:, want: partner_want2, stock: applicant_stock2) }
+  let(:other_partner_dealing) { create(:dealing, applicant:, want: other_partner_want, stock: partner_stock2) }
 
   describe 'POST /v1/dealings' do
     subject(:create_dealings) { post v1_dealings_path, params:, headers: }
@@ -25,36 +44,40 @@ RSpec.describe 'V1::Dealings' do
     let(:user) { applicant }
 
     context 'when applicant applicate dealing' do
-      let(:params) do
-        {
-          my_want_id: applicant_want.id,
-          your_stock_id: partner_stock.id,
-          your_want_id: partner_want.id,
-          my_stock_id: applicant_stock.id,
-        }
-      end
+      context 'when concerned poeple are two' do
+        let(:params) do
+          {
+            my_want_id: applicant_want1.id,
+            your_stock_id: partner_stock1.id,
+            your_want_id: partner_want1.id,
+            my_stock_id: applicant_stock1.id,
+          }
+        end
 
-      it 'returns 200' do
-        allow(Dealing).to receive(:applicate)
-        create_dealings
-        expect(response).to have_http_status(:ok)
-        expect(Dealing).to have_received(:applicate).with(
-          applicant.id,
-          partner.id,
-          applicant_want.id,
-          partner_stock.id,
-          partner_want.id,
-          applicant_stock.id,
-        )
-      end
-
-      context 'but partner stock is already trading' do
-        before { partner_stock.update(status: :trading) }
-
-        it 'returns 404' do
+        it 'returns 200' do
           create_dealings
-          expect(response).to have_http_status(:not_found)
-          expect(response.parsed_body['errors']).to eq({ 'message' => '状態が更新されてしまいました' })
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'create two dealings' do
+          expect do
+            create_dealings
+          end.to change {
+                   Dealing.where(want_id: applicant_want1.id, stock_id: partner_stock1.id).count
+                 }.from(0).to(1).and \
+                   change {
+                     Dealing.where(want_id: partner_want1.id, stock_id: applicant_stock1.id).count
+                   }.from(0).to(1)
+        end
+
+        context 'but partner stock is already trading' do
+          before { partner_stock1.update(status: :trading) }
+
+          it 'returns 404' do
+            create_dealings
+            expect(response).to have_http_status(:not_found)
+            expect(response.parsed_body['errors']).to eq({ 'message' => '状態が更新されてしまいました' })
+          end
         end
       end
     end
@@ -63,33 +86,83 @@ RSpec.describe 'V1::Dealings' do
   describe 'PATCH /v1/dealings/:id/approve' do
     subject(:approve_dealings) { patch approve_v1_dealing_path(dealing.id), headers: }
 
-    let(:dealing) do
-      create(
-        :dealing,
-        applicant_want:,
-        partner_stock:,
-        partner_want:,
-        applicant_stock:,
-      )
-    end
+    context 'when concerned people are two' do
+      let(:dealing) { partner_dealing1 }
 
-    context 'when partner approve dealing' do
-      let(:user) { partner }
+      before do
+        applicant_dealing1
+        partner_dealing1
 
-      it 'returns 200' do
-        approve_dealings
-        expect(response).to have_http_status(:ok)
-        expect(dealing.reload).to be_approval
+        applicant_dealing1.update(dealing: partner_dealing1)
+        partner_dealing1.update(dealing: applicant_dealing1)
       end
+
+      context 'when partner approve dealing' do
+        let(:user) { partner }
+
+        it 'returns 200' do
+          approve_dealings
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'update dealing status to trading' do
+          approve_dealings
+          expect(applicant_dealing1.reload).to be_trading
+          expect(partner_dealing1.reload).to be_trading
+        end
+      end
+
+      # context 'when applicant approve dealing' do
+      #   let(:user) { applicant }
+
+      #   it 'returns 403' do
+      #     approve_dealings
+      #     expect(response).to have_http_status(:forbidden)
+      #     expect(applicant_dealing1.reload).to be_application
+      #     expect(partner_dealing1.reload).to be_application
+      #   end
+      # end
     end
 
-    context 'when other than partner approve dealing' do
-      let(:user) { applicant }
+    context 'when concerned people are three or more' do
+      let(:dealing) { partner_dealing2 }
 
-      it 'returns 403' do
-        approve_dealings
-        expect(response).to have_http_status(:forbidden)
-        expect(dealing.reload).to be_application
+      before do
+        applicant_dealing2
+        partner_dealing2
+        other_partner_dealing
+
+        applicant_dealing2.update(dealing: other_partner_dealing)
+        partner_dealing2.update(dealing: applicant_dealing2)
+        other_partner_dealing.update(dealing: partner_dealing2)
+      end
+
+      context 'when partner approve dealing' do
+        let(:user) { partner }
+
+        it 'returns 200' do
+          approve_dealings
+          expect(response).to have_http_status(:ok)
+        end
+
+        context 'when all partner approve dealing' do
+          it 'update all dealing status to trading' do
+            other_partner_dealing.approve
+            approve_dealings
+            expect(applicant_dealing2.reload).to be_trading
+            expect(partner_dealing2.reload).to be_trading
+            expect(other_partner_dealing.reload).to be_trading
+          end
+        end
+
+        context 'when all partner does not approve dealing' do
+          it 'does not update dealing status to dealing' do
+            approve_dealings
+            expect(applicant_dealing2.reload).to be_application
+            expect(partner_dealing2.reload).to be_approval
+            expect(other_partner_dealing.reload).to be_application
+          end
+        end
       end
     end
   end
@@ -97,32 +170,98 @@ RSpec.describe 'V1::Dealings' do
   describe 'DELETE /v1/dealings/:id' do
     subject(:deny_dealings) { delete v1_dealing_path(dealing.id), headers: }
 
-    let(:dealing) do
-      create(
-        :dealing,
-        applicant_want:,
-        partner_stock:,
-        partner_want:,
-        applicant_stock:,
-      )
-    end
+    context 'when concerned people are two' do
+      let(:dealing) { partner_dealing1 }
 
-    context 'when partner deny dealing' do
-      let(:user) { partner }
+      before do
+        applicant_dealing1
+        partner_dealing1
 
-      it 'returns 200' do
-        deny_dealings
-        expect(response).to have_http_status(:ok)
+        applicant_dealing1.update(dealing: partner_dealing1)
+        partner_dealing1.update(dealing: applicant_dealing1)
       end
+
+      context 'when partner deny dealing' do
+        let(:user) { partner }
+
+        it 'returns 200' do
+          deny_dealings
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'delete dealing' do
+          deny_dealings
+          expect(Dealing.count).to be_zero
+          expect(applicant_want1.reload).to be_untrading
+          expect(partner_stock1.reload).to be_untrading
+          expect(partner_want1.reload).to be_untrading
+          expect(applicant_stock1.reload).to be_untrading
+        end
+      end
+
+      # context 'when applicant deny dealing' do
+      #   let(:user) { applicant }
+
+      #   it 'returns 403' do
+      #     deny_dealings
+      #     expect(response).to have_http_status(:forbidden)
+      #   end
+      # end
     end
 
-    context 'when other than partner deny dealing' do
-      let(:user) { applicant }
+    context 'when concerned people are three or more' do
+      shared_examples 'deny dealing' do
+        it 'returns 200' do
+          expect(response).to have_http_status(:ok)
+        end
 
-      it 'returns 403' do
-        deny_dealings
-        expect(response).to have_http_status(:forbidden)
-        expect(dealing.reload).to be_application
+        it 'delete dealing and update want and stock status to untrading' do
+          expect(Dealing.count).to be_zero
+          expect(applicant_want2.reload).to be_untrading
+          expect(other_partner_stock.reload).to be_untrading
+          expect(partner_want2.reload).to be_untrading
+          expect(applicant_stock2.reload).to be_untrading
+          expect(other_partner_want.reload).to be_untrading
+          expect(partner_stock2.reload).to be_untrading
+        end
+      end
+
+      context 'when partner deny dealing' do
+        let(:user) { partner }
+        let(:dealing) { partner_dealing2 }
+
+        context 'when no partner approve dealing' do
+          before do
+            applicant_dealing2
+            partner_dealing2
+            other_partner_dealing
+
+            applicant_dealing2.update(dealing: other_partner_dealing)
+            partner_dealing2.update(dealing: applicant_dealing2)
+            other_partner_dealing.update(dealing: partner_dealing2)
+
+            deny_dealings
+          end
+
+          it_behaves_like 'deny dealing'
+        end
+
+        context 'when other partner approve dealing' do
+          before do
+            applicant_dealing2
+            partner_dealing2
+            other_partner_dealing
+
+            applicant_dealing2.update(dealing: other_partner_dealing)
+            partner_dealing2.update(dealing: applicant_dealing2)
+            other_partner_dealing.update(dealing: partner_dealing2)
+            other_partner_dealing.approve
+
+            deny_dealings
+          end
+
+          it_behaves_like 'deny dealing'
+        end
       end
     end
   end
